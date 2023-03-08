8e5e7256f4d60e838903652cc16677b418e2c13f
Javier <92640479+divelz@users.noreply.github.com>
2023-03-08 05:02:47 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Script-Python-AutomaticWriting
+
+Project #4 with Python. Code to write automatically.
+
+## To use the code
+None
+
+## Images of the graphical interface
+
+**Image 1**
+
+![Sample image of the program](Image1.png)
+
+**Image 2**
+
+![Sample image of the program](Image2.png)
+
+<i>Best respect !!! </i>
+
+**<cite>Author: Francisco Velez</cite>**
---
+++ main.py
@@ -0,0 +1,28 @@
+import pyautogui as r
+
+print("")
+r.displayMousePosition()
+
+print("\n Digite en el siguiente formato (x-y)...")
+
+x, y = (input(" >>> Digite cordenadas (x, y): ")).split("-")
+
+while True:
+    opc = input("\n >>> ")
+    
+    if opc == "1":
+        txt = "Hola Mundo!"
+        r.moveTo(int(x.strip()), int(y.strip()), 0.3)
+        r.click()
+        r.write(txt, 0.05)
+
+    if opc == "go":
+        txt = ""
+        with open("./text.txt", "r") as f: txt = f.read() 
+        r.moveTo(int(x.strip()), int(y.strip()), 0.3)
+        r.click()
+        r.write(txt, 0.05)
+
+    if opc == "slr" or opc == "exit": break
+
+#* Author: Francisco Velez
